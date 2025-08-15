669768732ba692e7759c06c4c9cecbe501c25b2f
MartinOM321753 <133397788+MartinOM321753@users.noreply.github.com>
2025-08-15 10:03:58 -0600

    [ADD]se implemento role-based routing con `ProtectedRoute` y `PublicRoute`.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { AuthProvider } from "./auth/AuthContext"
 import AppRouter from "./router/AppRouter.jsx"
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-       
-      </AuthProvider>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <AuthProvider>
+                <div className="bg-primary text-white p-0">
+                    <AppRouter />
+                </div>
+            </AuthProvider>
+        </BrowserRouter>
+    )
 }
 
 export default App
